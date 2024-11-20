88a1b377ac305784153bafaa7d0ce8ed7c760e01
s0ymigue <pemm080223hdfrnga2@cbtis217.edu.mx>
2024-11-20 14:42:03 +0100

    falta codigo

---
+++ c_imaginar/c_imaginar/TAPETES.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC35
+ * Date: 20/11/2024
+ * Time: 02:12 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace c_imaginar
+{
+	/// <summary>
+	/// Description of TAPETES.
+	/// </summary>
+	public partial class TAPETES : Form
+	{
+		public TAPETES()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ c_imaginar/c_imaginar/UserControl1.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC35
+ * Date: 20/11/2024
+ * Time: 01:45 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace c_imaginar
+{
+	/// <summary>
+	/// Description of UserControl1.
+	/// </summary>
+	public partial class UserControl1 : UserControl
+	{
+		public UserControl1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
